6f600f2d82a77e49ae4e5260b035a96ece5782a9
ZairaCabrera <127335893+ZairaCabrera@users.noreply.github.com>
2025-02-02 12:31:00 +0100

    modificado path Config.php

--- app/libs/Config.php
+++ app/libs/Config.php
@@ -2,11 +2,11 @@
 
 class Config {
     public static $mvc_bd_hostname = "localhost";
-    public static $mvc_bd_nombre = "biblioteca";
+    public static $mvc_bd_nombre = "eventos";
     public static $mvc_bd_usuario = "root";
     public static $mvc_bd_clave = "";
     public static $mvc_vis_css = "estilo.css";
-    public static $vista = __DIR__ . '/../templates/inicio.php';
-    public static $menu = __DIR__ . '/../templates/menuInvitado.php';
+    public static $vista = __DIR__ . '/../../web/templates/inicio.php';
+    public static $menu = __DIR__ . '/../../web/templates/menuInvitado.php';
 }
 ?>
